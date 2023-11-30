9316446b78af33d316159bd5e037fa2c3162fbf5
KD-OO1 <143240008+KD-OO1@users.noreply.github.com>
2023-11-30 16:22:35 +0530

    Add files via upload

---
+++ Object detection/main.py
@@ -0,0 +1,60 @@
+import cv2
+from random import randint
+
+# Load YOLO model
+dnn = cv2.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
+model = cv2.dnn_DetectionModel(dnn)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+# Load classes
+with open('classes.txt') as f:
+    classes = f.read().strip().splitlines()
+
+# Open video file
+video_file_path = '/Users/dev/Documents/pr/Object detection/Video.mp4'  # Replace with your video file path
+capture = cv2.VideoCapture(video_file_path)
+
+# Set frame dimensions (optional)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+color_map = {}
+
+while True:
+    # Frame capture
+    ret, frame = capture.read()
+
+    if not ret:
+        # End of the video
+        break
+
+    frame = cv2.flip(frame, 1)
+
+    # Object detection
+    class_ids, confidences, boxes = model.detect(frame)
+    for id, confidence, box in zip(class_ids, confidences, boxes):
+        x, y, w, h = box
+        obj_class = classes[id]
+
+        if obj_class not in color_map:
+            color = (randint(0, 255), randint(0, 255), randint(0, 255))
+            color_map[obj_class] = color
+        else:
+            color = color_map[obj_class]
+
+        cv2.putText(frame, f'{obj_class.title()} {format(confidence, ".2f")}', (x, y-10), cv2.FONT_HERSHEY_DUPLEX, 1, color, 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+    cv2.imshow('Video Capture', frame)
+    key = cv2.waitKey(1)
+
+    # Key event handling
+    if key == 27:  # Esc key to exit
+        break
+
+    elif key == 13:  # Enter key to reset colors
+        color_map = {}
+
+# Release resources
+capture.release()
+cv2.destroyAllWindows()
